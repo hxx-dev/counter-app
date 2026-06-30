76e9d67780de75b374dafdbafcc3a22d38001b25
hxx-dev <red05077@gmail.com>
2026-06-30 14:43:49 +0900

    :sparkles:UI: UI 위치 크기 조정 및 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import styled from "styled-components";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
+
+//컨테이너로 감싸 위치 ,크기 조정
+const Container = styled.div` 
+  margin: 0 auto;
+  width: 400px;
+`;
 
 function App() {
-  return <></>;
+  return (
+    <Container>
+      <h1>Simple Counter</h1>
+      <Viewer />
+      <Controller />
+    </Container>
+  );
 }
 
 export default App;
